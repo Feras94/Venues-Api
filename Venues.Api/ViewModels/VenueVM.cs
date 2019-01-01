bb2409726a055b300796166111da2fe4c1a431de
Feras Dawod <feras.da94@gmail.com>
2019-01-01 22:31:58 +0200

    Add Venue view model file

---
+++ Venues.Api/ViewModels/VenueVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Venues.Api.ViewModels
+{
+    public class VenueVM
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("capacity")]
+        public int Capacity { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
